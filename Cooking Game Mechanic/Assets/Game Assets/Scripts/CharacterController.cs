9338cd774c6a6e922eab6e47b85dbc3f9bc41455
LukeJansen <lpjansen@live.co.uk>
2018-10-16 02:05:37 +0100

    Basic Implementation of Mechanic
    Created test world and created smooth look and movement.

---
+++ Cooking Game Mechanic/Assets/Game Assets/Scripts/CharacterController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour {
+
+    public float movementSmoothing = 0.5f;
+
+    public Transform mainTransform;
+    public Transform cameraTranform;
+
+	// Use this for initialization
+	void Start () {
+        Cursor.lockState = CursorLockMode.Locked;
+
+        Debug.Log(Vector3.up);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        if (mouseX != 0 || mouseY != 0)
+        {
+            cameraTranform.Rotate(new Vector3(-mouseY, mouseX, 0));
+
+            cameraTranform.eulerAngles = new Vector3(cameraTranform.eulerAngles.x, cameraTranform.eulerAngles.y, 0);            
+        }
+
+        mainTransform.position += (cameraTranform.forward * Input.GetAxis("Vertical")) * movementSmoothing;
+        mainTransform.position += (cameraTranform.right * Input.GetAxis("Horizontal")) * movementSmoothing;
+        mainTransform.position = new Vector3(mainTransform.position.x, 5, mainTransform.position.z);
+	}
+}
