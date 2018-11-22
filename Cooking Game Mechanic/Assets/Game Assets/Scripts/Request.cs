249b305458bc2dba878a9749f478ebf6c24b79d9
LukeJansen <lpjansen@live.co.uk>
2018-11-22 15:06:20 +0000

    Request System Implementation Started

---
+++ Cooking Game Mechanic/Assets/Game Assets/Scripts/Request.cs
@@ -0,0 +1,69 @@
+﻿using Assets.Game_Assets.Scripts;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Request{
+
+    private RecipeBook recipeBook;
+    private Recipe recipe;
+    private int time;
+    private int timeLeft;
+
+    private GameObject requestObject;
+    private Slider timeLeftObject;
+    private Image timeLeftColour;
+    private Image foodImage;
+    private Text foodText;
+
+    public Request(Recipe recipe, int time, GameObject requestObject)
+    {
+        this.recipeBook = new RecipeBook();
+        this.recipe = recipe;
+        this.time = time;
+        this.timeLeft = time;
+        this.requestObject = requestObject;
+
+        SetUp();
+    }
+
+    private void SetUp()
+    {
+        timeLeftObject = requestObject.transform.Find("Time Left").GetComponent<Slider>();
+        timeLeftColour = requestObject.transform.Find("Time Left").transform.Find("Fill Area").transform.Find("Fill").GetComponent<Image>();
+        foodImage = requestObject.transform.Find("Food").GetComponent<Image>();
+        foodText = requestObject.transform.Find("Text").GetComponent<Text>();
+
+        Debug.Log(recipeBook.foodItems[recipe.meal].name);
+        foodImage.sprite = Resources.Load<Sprite>(recipeBook.foodItems[recipe.meal].name);
+        foodText.text = recipeBook.foodItems[recipe.meal].name;
+    }
+
+    public void TakeSecond()
+    {
+        Color color;
+
+        if (timeLeft > 0)
+        {
+            timeLeft -= 1;
+
+            float value = (float)timeLeft / time;
+            timeLeftObject.value = value;
+
+            Debug.Log(1 - value);
+
+            if ((1 - value) < 0.5)
+            {
+                color = Color.Lerp(Color.green, Color.yellow, Mathf.Lerp(0, 1, Mathf.InverseLerp(0, 0.5f, (1- value))));
+            }
+            else
+            {
+                color = Color.Lerp(Color.yellow, Color.red, Mathf.Lerp(0, 1, Mathf.InverseLerp(0.5f, 1, (1-value))));
+            }
+            timeLeftColour.color = color;
+        }
+    }
+
+}
+
--- Cooking Game Mechanic/Assets/Game Assets/Scripts/ServingBehaviour.cs
+++ Cooking Game Mechanic/Assets/Game Assets/Scripts/ServingBehaviour.cs
@@ -5,15 +5,55 @@ using UnityEngine;
 
 public class ServingBehaviour : MonoBehaviour {
 
+    public GameObject uiPrefab;
+    public RectTransform canvas;
+
+    private RecipeBook recipeBook;
+    private int orderLimit;
+    private List<Request> currentOrders;
+    private System.Random random;
+    private float lastSecond;
+
+
 	// Use this for initialization
 	void Start () {
-		
+        recipeBook = new RecipeBook();
+        currentOrders = new List<Request>();
+        random = new System.Random();
+        lastSecond = Time.time;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        orderLimit = Mathf.FloorToInt(Time.time / 120f) + 1;
+
+        ManageOrders();
+        ManageTime();
 	}
+
+    private void ManageOrders()
+    {
+        if (currentOrders.Count < orderLimit)
+        {
+            currentOrders.Add(
+                new Request(recipeBook.recipeItems[random.Next(recipeBook.recipeItems.Count)],
+                random.Next(30, 60), Instantiate(uiPrefab, canvas)));
+            
+        }
+    }
+
+    private void ManageTime()
+    {
+        if (Time.time - lastSecond >= 1)
+        {
+            foreach (Request request in currentOrders)
+            {
+                request.TakeSecond();
+            }
+
+            lastSecond = Time.time;
+        }
+    }
 
     public void TakeMeal(Food meal)
     {
